a372f099d19be85e53318db3b930571adb4383b3
Benosn-Hou <112306055@g.nccu.edu.tw>
2024-12-19 15:41:51 +0800

    keyword'S data

--- FinalProject_Book/src/main/java/org/finalproject/book/Keyword/Keyword.java
+++ FinalProject_Book/src/main/java/org/finalproject/book/Keyword/Keyword.java
@@ -6,21 +6,45 @@ public class Keyword {
 	public String name;
 	public int count;
 	public double weight;
+	private ArrayList<Keyword> bookList;
 	
 	public Keyword(String name,float weight){
 		this.name = name;
 		this.count = 0;
 		this.weight = weight;
-	}
-	public Keyword() {
-		// TODO Auto-generated constructor stub
+		this.bookList = new ArrayList<>();
 	}
 	
-
 	//@Override
 	//public String toString(){
 	//	return "["+name+","+count+","+weight+"]";
 	//}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	
+	public void setBookList() {
+		bookList.add(new Keyword("書籍", 10));
+		bookList.add(new Keyword("出版社", 10));
+		bookList.add(new Keyword("書評", 6));
+		bookList.add(new Keyword("內容簡介", 7));
+		bookList.add(new Keyword("作者", 7));
+		bookList.add(new Keyword("書本主題", 7));
+		bookList.add(new Keyword("書本類型", 8));
+		bookList.add(new Keyword("書店", 7));
+		bookList.add(new Keyword("選書", 6));
+		bookList.add(new Keyword("贈書", 4));
+		bookList.add(new Keyword("小說", 5));
+		bookList.add(new Keyword("書摘", 7));
+		bookList.add(new Keyword("電子書", 5));
+		bookList.add(new Keyword("文學", 5));
+		bookList.add(new Keyword("節錄", 5));
+	}
 	
     public int getCount()
     {
